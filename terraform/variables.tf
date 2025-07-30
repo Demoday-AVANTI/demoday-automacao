f3dde6b518edfdb72dc9cb8e003eab0b1c6defad
gaabs16 <gbhernandes@hotmail.com>
2025-07-30 19:10:55 -0300

    test: ajustando terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,14 @@
 variable "cluster_name" {
-  description = "The name for the EKS cluster"
   default     = "demo-cluster"
   type        = string
 }
 
 variable "aws_region" {
-  description = "The AWS region to deploy resources"
   default     = "us-east-1"
   type        = string
 }
 
 variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
   default     = "10.0.0.0/16"
   type        = string
 }
